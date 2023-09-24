9bcb190dafbab83d2b8538c2925545fcb610b87a
Yorgos Kakavas <yorgos.kakavas@proton.me>
2023-09-24 17:59:47 +0300

    Performance opts.

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,5 +1,6 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback, useMemo } from 'react';
 import DOMPurify from 'dompurify';
+// import debounce from 'lodash/debounce';
 import KeyValue from './KeyValue';
 import useClipboardCopy from '../hooks/useClipboardCopy';
 
@@ -19,60 +20,64 @@ const Display: React.FC<DisplayProps> = ({
   onEdit,
 }) => {
   const outerRef = useRef<HTMLPreElement | null>(null);
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const { copyToClipboard, copied } = useClipboardCopy(outerRef);
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string>(DOMPurify.sanitize(JSON.stringify(data, null, 2)));
   const [isValidJSON, setIsValidJSON] = useState<boolean>(true);
 
+  const parsedDataRef = useMemo(() => data, [data]);
+
   useEffect(() => {
     setInputValue(DOMPurify.sanitize(JSON.stringify(data, null, 2)));
   }, [data]);
 
-  function validateJSON(jsonString: string): boolean {
+  useEffect(() => {
+    if (isEditing && textareaRef.current) {
+      textareaRef.current.focus();
+    }
+  }, [isEditing]);
+
+  const sanitizedInputValue = useMemo(() => DOMPurify.sanitize(inputValue), [inputValue]);
+
+  const validateJSON = useCallback(() => {
     try {
-      const sanitizedInputValue = DOMPurify.sanitize(jsonString);
-      validateJSON.parsed = JSON.parse(sanitizedInputValue);
+      JSON.parse(sanitizedInputValue);
       setIsValidJSON(true);
-      return true;
     } catch (e) {
       setIsValidJSON(false);
-      validateJSON.parsed = data;
-      return false;
     }
-  }
-  validateJSON.parsed = data;
+  }, [sanitizedInputValue]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleInputChange = useCallback((event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputValue(event.target.value);
-    validateJSON(event.target.value);
-  };
+    validateJSON();
+  }, [validateJSON]);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    validateJSON(inputValue);
     if (isValidJSON) {
-      setIsEditing(false);
-      onEdit?.(validateJSON.parsed);
+      const parsedData = JSON.parse(sanitizedInputValue);
+      onEdit?.(parsedData);
     }
-  };
+    setIsEditing(false);
+  }, [onEdit, sanitizedInputValue, isValidJSON]);
 
-  const handleCancel = () => {
-    // console.log('inputValue', JSON.parse(inputValue)?.hasError , JSON.parse(JSON.stringify(data, null, 2))?.hasError);
-    const dataToCompare = JSON.stringify(data, null, 2);
+  const handleCancel = useCallback(() => {
+    const dataToCompare = JSON.stringify(parsedDataRef, null, 2);
     if (inputValue !== dataToCompare) {
       if (window.confirm('Are you sure you want to discard your changes?')) {
-        setIsEditing(false);
         setInputValue(dataToCompare);
         setIsValidJSON(true);
+        setIsEditing(false);
       }
     } else {
       setIsEditing(false);
       setIsValidJSON(true);
     }
-  };
+  }, [inputValue, parsedDataRef]);
 
-  const handleFileDownload = () => {
-    const sanitizedInputValue = DOMPurify.sanitize(inputValue);
+  const handleFileDownload = useCallback(() => {
     const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(sanitizedInputValue);
     const downloadAnchorNode = document.createElement('a');
     downloadAnchorNode.setAttribute("href", dataStr);
@@ -80,7 +85,7 @@ const Display: React.FC<DisplayProps> = ({
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
-  };
+  }, [sanitizedInputValue]);
 
   return (
     <div className="json-display">
@@ -99,6 +104,14 @@ const Display: React.FC<DisplayProps> = ({
           <button onClick={handleFileDownload} title="Download JSON data">
             Download
           </button>
+          {isEditing && (
+            <div className="textarea-info">
+              <hr />
+              <div className={`character-count ${isValidJSON ? '' : 'invalid'}`}>{inputValue.length} / {'1 billion chars'}</div>
+              {isValidJSON ? <div className="success-message">Valid JSON</div> : <div className="error invalid">Invalid JSON</div>}
+              <hr />
+            </div>
+          )}
           {copied && <div className="copied-indicator">Copied!</div>}
         </div>
         <pre className="json-container" ref={outerRef}>
@@ -114,21 +127,17 @@ const Display: React.FC<DisplayProps> = ({
           )}
           {isEditing && (
             <textarea
+              ref={textareaRef}
               value={inputValue}
               onChange={handleInputChange}
               placeholder="Enter JSON data here"
               title="Edit JSON data"
               rows={40}
               style={{width: '100%'}}
+              maxLength={1000000000}
             />
           )}
         </pre>
-        {isEditing && (
-          <div className="textarea-info">
-            <div className={`character-count ${isValidJSON ? '' : 'invalid'}`}>{inputValue.length} / {200000}</div>
-            {isValidJSON ? <div className="success-message">Valid JSON</div> : <div className="error invalid">Invalid JSON</div>}
-          </div>
-        )}
       </form>
     </div>
   );
